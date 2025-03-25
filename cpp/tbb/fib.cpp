efee22b2d2ce49cc792c4e2cccb701b9e84904e7
tzcnt <trailingzerocount@gmail.com>
2025-03-24 20:55:44 -0700

    add cpp/TBB library

---
+++ cpp/tbb/fib.cpp
@@ -0,0 +1,78 @@
+// An implementation of the recursive fork fibonacci parallelism test.
+
+// Adapted from https://github.com/tzcnt/tmc-examples/blob/main/examples/fib.cpp
+// Original author: tzcnt
+// Unlicense License
+// This is free and unencumbered software released into the public domain.
+
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+#include <tbb/tbb.h>
+
+#include <chrono>
+#include <cinttypes>
+#include <cstdio>
+
+static int thread_count = std::thread::hardware_concurrency() / 2;
+static const size_t iter_count = 1;
+
+size_t fibonacci(size_t n) {
+  if (n < 2)
+    return n;
+
+  size_t a, b;
+
+  tbb::parallel_invoke(
+    [&] { a = fibonacci(n - 1); }, [&] { b = fibonacci(n - 2); }
+  );
+
+  return a + b;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    printf("Usage: fib <n-th fibonacci number requested>\n");
+    exit(0);
+  }
+  size_t n = static_cast<size_t>(atoi(argv[1]));
+
+  std::printf("threads: %d\n", thread_count);
+  tbb::task_arena arena(thread_count);
+  std::printf("results:\n");
+
+  size_t result;
+  arena.execute([&] { result = fibonacci(n); }); // warmup
+
+  auto startTime = std::chrono::high_resolution_clock::now();
+
+  for (size_t i = 0; i < iter_count; ++i) {
+    arena.execute([&] { result = fibonacci(n); }); // warmup
+    std::printf("  - %" PRIu64 "\n", result);
+  }
+
+  auto endTime = std::chrono::high_resolution_clock::now();
+  auto totalTimeUs =
+    std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+  std::printf("runs:\n");
+  std::printf("  - iteration_count: %" PRIu64 "\n", iter_count);
+  std::printf("    duration: %" PRIu64 " us\n", totalTimeUs.count());
+}
---
+++ cpp/tbb/skynet.cpp
@@ -0,0 +1,98 @@
+// The skynet benchmark as described here:
+// https://github.com/atemerev/skynet
+
+// Adapted from
+// https://github.com/tzcnt/tmc-examples/blob/main/examples/skynet/main.cpp
+// Original author: tzcnt
+// Unlicense License
+// This is free and unencumbered software released into the public domain.
+
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+#include <oneapi/tbb/parallel_invoke.h>
+#include <tbb/tbb.h>
+
+#include <chrono>
+#include <cinttypes>
+#include <cstdio>
+#include <ranges>
+
+static int thread_count = std::thread::hardware_concurrency() / 2;
+static const size_t iter_count = 1;
+
+template <size_t DepthMax> size_t skynet_one(size_t BaseNum, size_t Depth) {
+  if (Depth == DepthMax) {
+    return BaseNum;
+  }
+  size_t depthOffset = 1;
+  for (size_t i = 0; i < DepthMax - Depth - 1; ++i) {
+    depthOffset *= 10;
+  }
+
+  std::array<size_t, 10> results;
+
+  auto tasks = std::ranges::views::iota(0UL, 10UL);
+  tbb::parallel_for_each(
+    tasks.begin(), tasks.end(),
+    [=, &results](size_t const& idx) {
+      results[idx] =
+        skynet_one<DepthMax>(BaseNum + depthOffset * idx, Depth + 1);
+    }
+  );
+
+  size_t count = 0;
+  for (size_t idx = 0; idx < 10; ++idx) {
+    count += results[idx];
+  }
+  return count;
+}
+template <size_t DepthMax> void skynet() {
+  size_t count = skynet_one<DepthMax>(0, 0);
+  if (count != 4999999950000000) {
+    std::printf("ERROR: wrong result - %" PRIu64 "\n", count);
+  }
+}
+
+template <size_t Depth = 6> void loop_skynet() {
+  std::printf("runs:\n");
+  for (size_t j = 0; j < iter_count; ++j) {
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    skynet<Depth>();
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto execDur = endTime - startTime;
+    auto totalTimeUs = std::chrono::duration_cast<std::chrono::microseconds>(
+      endTime - startTime
+    );
+    std::printf("  - iteration_count: %" PRIu64 "\n", iter_count);
+    std::printf("    duration: %" PRIu64 " us\n", totalTimeUs.count());
+  }
+}
+
+int main() {
+  std::printf("threads: %d\n", thread_count);
+  tbb::task_arena arena(thread_count);
+
+  arena.execute(skynet<8>); // warmup
+  arena.execute(loop_skynet<8>);
+}
