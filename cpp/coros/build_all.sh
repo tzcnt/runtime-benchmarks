4eb25b33043fe3faeb1bf1917c1e33e2cfeb0fb0
tzcnt <trailingzerocount@gmail.com>
2025-03-31 08:00:00 -0700

    update build scripts

--- cpp/coros/build_all.sh
+++ cpp/coros/build_all.sh
@@ -1,2 +1,3 @@
 cmake --preset clang-linux-release .
-cmake --build ./build --parallel 16 --target all
+cmake --build ./build --parallel 16 --target bench-fib matmul nqueens skynet
+mv ./build/bench-fib ./build/fib
