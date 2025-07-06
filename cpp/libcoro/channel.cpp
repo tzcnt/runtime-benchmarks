1586994cb532a87dc7eb52a3eae1651d419115c0
tzcnt <trailingzerocount@gmail.com>
2025-07-06 09:30:59 -0700

    WIP channel bench

--- cpp/libcoro/channel.cpp
+++ cpp/libcoro/channel.cpp
@@ -25,10 +25,8 @@
 // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-#define TMC_IMPL
-
-#include "tmc/all_headers.hpp"
-#include "tmc/asio/ex_asio.hpp"
+#include "coro/coro.hpp"
+#include "coro/thread_pool.hpp"
 
 #include <chrono>
 #include <cinttypes>
@@ -38,66 +36,75 @@
 
 static size_t thread_count = std::thread::hardware_concurrency() / 2;
 static size_t producer_count = 4;
-static size_t consumer_count = 1;
+static size_t consumer_count = 4;
 static const size_t iter_count = 1;
 
 static constexpr size_t element_count = 10000000;
 
 static size_t expected_sum;
 
-using token = tmc::chan_tok<size_t>;
-
-tmc::task<void> producer(token chan, size_t count, size_t base) {
-  for (size_t i = 0; i < count; ++i) {
-    bool ok = chan.post(base + i);
-    assert(ok);
-  }
-  co_return;
-}
-
 struct result {
   size_t count;
   size_t sum;
 };
 
-tmc::task<result> consumer(token chan) {
+coro::task<result> producer(
+  coro::queue<size_t>& chan, size_t count, size_t base,
+  std::atomic<size_t>& countDown, coro::thread_pool& tp
+) {
+  co_await tp.schedule();
+  for (size_t i = 0; i < count; ++i) {
+    auto result = co_await chan.push(base + i);
+    assert(result == coro::queue_produce_result::produced);
+  }
+
+  // Since we can't fork and await them separately in the main task, one of the
+  // producers has to handle shutting down the channel.
+  if (countDown.fetch_sub(1) == 1) {
+    co_await chan.shutdown_notify_waiters_drain(tp);
+  }
+  co_return {};
+}
+
+coro::task<result> consumer(coro::queue<size_t>& chan, coro::thread_pool& tp) {
+  co_await tp.schedule();
   size_t count = 0;
   size_t sum = 0;
-  auto data = co_await chan.pull();
-  while (data.has_value()) {
+  auto data = co_await chan.pop();
+  while (data) {
     ++count;
     sum += data.value();
-    data = co_await chan.pull();
+    data = co_await chan.pop();
   }
   co_return result{count, sum};
 }
 
-static tmc::task<size_t> do_bench() {
-  auto chan = tmc::make_channel<size_t>();
+static coro::task<size_t> do_bench(coro::thread_pool& tp) {
+  auto chan = coro::queue<size_t>();
   size_t per_task = element_count / producer_count;
   size_t rem = element_count % producer_count;
-  std::vector<tmc::task<void>> prod(producer_count);
+
+  // I can't figure out how to fork the producers separately using libcoro.
+  // So I make the return type the same as the consumers and initiate them all
+  // at once.
+  std::vector<coro::task<result>> tasks(producer_count + consumer_count);
   size_t base = 0;
+  std::atomic<size_t> countDown = producer_count;
   for (size_t i = 0; i < producer_count; ++i) {
     size_t count = i < rem ? per_task + 1 : per_task;
-    prod[i] = producer(chan, count, base);
+    tasks[i] = producer(chan, count, base, countDown, tp);
     base += count;
   }
-  std::vector<tmc::task<result>> cons(consumer_count);
-  for (size_t i = 0; i < consumer_count; ++i) {
-    cons[i] = consumer(chan);
+  for (size_t i = producer_count; i < producer_count + consumer_count; ++i) {
+    tasks[i] = consumer(chan, tp);
   }
-  auto c = tmc::spawn_many(cons.data(), cons.size()).fork();
-  co_await tmc::spawn_many(prod.data(), prod.size());
-
-  co_await chan.drain();
-  auto consResults = co_await std::move(c);
+  auto results = co_await coro::when_all(std::move(tasks));
 
   size_t count = 0;
   size_t sum = 0;
-  for (size_t i = 0; i < consResults.size(); ++i) {
-    count += consResults[i].count;
-    sum += consResults[i].sum;
+  for (size_t i = 0; i < results.size(); ++i) {
+    count += results[i].return_value().count;
+    sum += results[i].return_value().sum;
   }
   if (count != element_count) {
     std::printf(
@@ -115,12 +122,6 @@ static tmc::task<size_t> do_bench() {
 int main(int argc, char* argv[]) {
   if (argc > 1) {
     thread_count = static_cast<size_t>(atoi(argv[1]));
-    auto c = thread_count / 2;
-    if (c == 0) {
-      c = 1;
-    }
-    producer_count = c;
-    consumer_count = c;
   }
   if (argc > 2) {
     producer_count = static_cast<size_t>(atoi(argv[2]));
@@ -130,8 +131,6 @@ int main(int argc, char* argv[]) {
     consumer_count = static_cast<size_t>(atoi(argv[3]));
   }
 
-  // TODO implement ex_asio / ex_cpu executor switch based on command line
-
   expected_sum = 0;
   for (size_t i = 0; i < element_count; ++i) {
     expected_sum += i;
@@ -139,36 +138,38 @@ int main(int argc, char* argv[]) {
 
   std::printf("threads: %zu\n", thread_count);
   std::printf("producers: %zu\n", producer_count);
-  std::printf("consumers: %zu\n", consumer_count);
-  tmc::cpu_executor().set_thread_count(thread_count).init();
-  tmc::asio_executor().init();
+  std::printf("threads: %zu\n", consumer_count);
+  coro::thread_pool tp{coro::thread_pool::options{
+    .thread_count = static_cast<uint32_t>(thread_count)
+  }};
 
-  tmc::ex_any* exec = false ? tmc::cpu_executor().type_erased()
-                            : tmc::asio_executor().type_erased();
+  return coro::sync_wait([](coro::thread_pool& tp) -> coro::task<int> {
+    co_await tp.schedule();
+    {
+      auto result = co_await do_bench(tp); // warmup
+      std::printf("output: %zu\n", result);
+    }
 
-  {
-    auto result = tmc::post_waitable(exec, do_bench()).get(); // warmup
-    std::printf("output: %zu\n", result);
-  }
+    auto startTime = std::chrono::high_resolution_clock::now();
 
-  auto startTime = std::chrono::high_resolution_clock::now();
+    for (size_t i = 0; i < iter_count; ++i) {
+      auto result = co_await do_bench(tp);
+    }
 
-  for (size_t i = 0; i < iter_count; ++i) {
-    auto result = tmc::post_waitable(exec, do_bench()).get();
-  }
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto totalTimeUs =
+      std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime)
+        .count();
 
-  auto endTime = std::chrono::high_resolution_clock::now();
-  auto totalTimeUs =
-    std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime)
-      .count();
-
-  size_t elementsPerSec = static_cast<size_t>(
-    static_cast<float>(element_count) * 1000000.0f /
-    static_cast<float>(totalTimeUs)
-  );
-  std::printf("runs:\n");
-  std::printf("  - iteration_count: %zu\n", iter_count);
-  std::printf("    elements: %zu\n", element_count);
-  std::printf("    duration: %zu us\n", totalTimeUs);
-  std::printf("    elements/sec: %zu\n", elementsPerSec);
+    size_t elementsPerSec = static_cast<size_t>(
+      static_cast<float>(element_count) * 1000000.0f /
+      static_cast<float>(totalTimeUs)
+    );
+    std::printf("runs:\n");
+    std::printf("  - iteration_count: %zu\n", iter_count);
+    std::printf("    elements: %zu\n", element_count);
+    std::printf("    duration: %zu us\n", totalTimeUs);
+    std::printf("    elements/sec: %zu\n", elementsPerSec);
+    co_return 0;
+  }(tp));
 }
