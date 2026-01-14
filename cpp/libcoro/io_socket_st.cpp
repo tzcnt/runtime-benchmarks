c3c701fc532ad1e1c4b48c99989a736e1f6e9016
tzcnt <trailingzerocount@gmail.com>
2026-01-13 19:36:31 -0800

    WIP

--- cpp/libcoro/io_socket_st.cpp
+++ cpp/libcoro/io_socket_st.cpp
@@ -26,39 +26,53 @@ const std::string static_request =
   "HEAD / HTTP/1.1\r\nHost: host:port\r\nConnection: close\r\n\r\n";
 
 struct result {
-  coro::net::send_status ec;
+  coro::net::send_status sc;
+  coro::net::recv_status rc;
   size_t recv_count;
 };
 
 // not safe to accept rvalue reference
 // have to accept value so that it gets moved when the coro is constructed
-coro::task<void>
-server_handler(auto Socket, size_t Count, coro::queue<result>& Results) {
-  char data[4096];
+coro::task<void> server_handler(
+  coro::net::tcp::client client, size_t Count, coro::queue<result>& Results
+) {
+  // Why a string? IDK but this is what libcoro's examples prefer
+  std::string data(4096, '\0');
   size_t i = 0;
   for (; i < Count; ++i) {
-    poll_status = co_await client.poll(coro::poll_op::read);
-    auto d = boost::asio::buffer(data);
-    auto [error, n] = co_await Socket.async_read_some(d, coro::aw_asio);
-    if (error) {
-      Results.post(result{error, i});
-      Socket.close();
-      co_return;
+    while (true) {
+      // Wait for data to be available to read.
+      co_await client.poll(coro::poll_op::read);
+      auto [rstatus, rspan] = client.recv(data);
+      switch (rstatus) {
+      case coro::net::recv_status::ok:
+        goto SEND;
+      case coro::net::recv_status::try_again:
+        break;
+      default:
+        co_await Results.emplace(coro::net::send_status::ok, rstatus, i);
+        co_return;
+      }
     }
-
-    auto d2 = boost::asio::buffer(static_response);
-    std::tie(error, n) =
-      co_await boost::asio::async_write(Socket, d2, coro::aw_asio);
-    if (error) {
-      Results.post(result{error, i});
-      Socket.close();
-      co_return;
-    }
+  SEND:
+    // Make sure the client socket can be written to.
+    co_await client.poll(coro::poll_op::write);
+    auto sspan = std::span<const char>{static_response};
+    coro::net::send_status sstatus;
+    do {
+      std::tie(sstatus, sspan) = client.send(sspan);
+      if (sstatus != coro::net::send_status::ok) {
+        co_await Results.emplace(sstatus, coro::net::recv_status::ok, i);
+        co_return;
+      }
+    } while (!sspan.empty());
   }
 
-  Socket.shutdown(tcp::socket::shutdown_both);
-  Socket.close();
-  Results.post(result{boost::system::error_code{}, i});
+  client.socket().shutdown();
+  client.socket().close();
+  co_await Results.emplace(
+    coro::net::send_status::ok, coro::net::recv_status::ok, i
+  );
 }
 
 static coro::task<void>
@@ -66,10 +80,30 @@ server(std::shared_ptr<coro::io_scheduler> executor, uint16_t Port) {
   executor->schedule();
   // TODO Replace this with async barrier
   auto finished_chan = coro::queue<result>();
-  coro::net::tcp::server server{scheduler};
-  tcp::acceptor acceptor(ex, {tcp::v4(), Port});
+  coro::net::tcp::server server{
+    executor, coro::net::tcp::server::options{.port = Port}
+  };
 
   for (size_t i = 0; i < CONNECTION_COUNT; ++i) {
+
+    // Wait for a new connection.
+    auto pstatus = co_await server.poll();
+    switch (pstatus) {
+    case coro::poll_status::event: {
+      auto client = server.accept();
+      if (client.socket().is_valid()) {
+        executor->spawn_detached(make_on_connection_task(std::move(client)));
+      } // else report error or something if the socket was invalid or could not
+        // be accepted.
+    } break;
+    case coro::poll_status::write:
+    case coro::poll_status::error:
+    case coro::poll_status::closed:
+    case coro::poll_status::timeout:
+    default:
+      co_return;
+    }
+
     auto [error, sock] = co_await acceptor.async_accept(coro::aw_asio);
     if (error) {
       std::terminate();
@@ -150,17 +184,19 @@ int main(int argc, char* argv[]) {
     CONNECTION_COUNT = static_cast<size_t>(atoi(argv[1]));
   }
 
-  auto server_executor =
-    coro::io_scheduler::make_shared(coro::io_scheduler::options{
+  auto server_executor = coro::io_scheduler::make_shared(
+    coro::io_scheduler::options{
       .execution_strategy =
         coro::io_scheduler::execution_strategy_t::process_tasks_inline
-    });
+    }
+  );
 
-  auto client_executor =
-    coro::io_scheduler::make_shared(coro::io_scheduler::options{
+  auto client_executor = coro::io_scheduler::make_shared(
+    coro::io_scheduler::options{
       .execution_strategy =
         coro::io_scheduler::execution_strategy_t::process_tasks_inline
-    });
+    }
+  );
 
   std::printf("serving on http://localhost:%d/\n", PORT);
 
