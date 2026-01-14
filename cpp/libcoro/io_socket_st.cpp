e9d6a133dc2cf72faa3dc77a227ed0794ae59ccf
tzcnt <trailingzerocount@gmail.com>
2026-01-13 20:39:27 -0800

    finish io_socket_st for libcoro

--- cpp/libcoro/io_socket_st.cpp
+++ cpp/libcoro/io_socket_st.cpp
@@ -2,6 +2,7 @@
 // Listens on http://localhost:55550/
 
 #include "coro/coro.hpp"
+#include "coro/net/connect.hpp"
 
 #include <cstddef>
 #include <cstdint>
@@ -76,51 +77,55 @@ coro::task<void> server_handler(
 }
 
 static coro::task<void>
-server(std::shared_ptr<coro::io_scheduler> executor, uint16_t Port) {
+server(std::unique_ptr<coro::io_scheduler>& executor, uint16_t Port) {
   executor->schedule();
-  // TODO Replace this with async barrier
   auto finished_chan = coro::queue<result>();
   coro::net::tcp::server server{
     executor, coro::net::tcp::server::options{.port = Port}
   };
 
   for (size_t i = 0; i < CONNECTION_COUNT; ++i) {
-
     // Wait for a new connection.
     auto pstatus = co_await server.poll();
     switch (pstatus) {
-    case coro::poll_status::event: {
+    case coro::poll_status::read: {
       auto client = server.accept();
       if (client.socket().is_valid()) {
-        executor->spawn_detached(make_on_connection_task(std::move(client)));
-      } // else report error or something if the socket was invalid or could not
-        // be accepted.
+        executor->spawn_detached(
+          server_handler(std::move(client), REQUEST_COUNT, finished_chan)
+        );
+      } else {
+        std::printf("server acceptor socket was invalid!\n");
+        std::terminate();
+      }
     } break;
     case coro::poll_status::write:
     case coro::poll_status::error:
     case coro::poll_status::closed:
     case coro::poll_status::timeout:
     default:
-      co_return;
-    }
-
-    auto [error, sock] = co_await acceptor.async_accept(coro::aw_asio);
-    if (error) {
+      std::printf("server acceptor error!\n");
       std::terminate();
     }
-    coro::spawn(server_handler(std::move(sock), REQUEST_COUNT, finished_chan))
-      .detach();
   }
   size_t total = 0;
   for (size_t i = 0; i < CONNECTION_COUNT; ++i) {
-    auto result = co_await finished_chan.pull();
+    auto result = co_await finished_chan.pop();
     if (!result.has_value()) {
+      std::printf("results channel closed prematurely!\n");
       std::terminate();
     }
-    // if (result.value().ec) {
-    //   auto msg = result.value().ec.message();
-    //   std::printf("FAIL in server: %s\n", msg.c_str());
-    // }
+    if (result.value().sc != coro::net::send_status::ok) {
+      auto err = result.value().sc;
+      std::printf("FAIL in server send. error code: %d\n", err);
+      std::terminate();
+    }
+    // Expect closed status from server, since client closes the connection
+    if (result.value().rc != coro::net::recv_status::closed) {
+      auto err = result.value().rc;
+      std::printf("FAIL in server recv. error code: %d\n", err);
+      std::terminate();
+    }
     total += result.value().recv_count;
   }
   if (total != REQUEST_COUNT) {
@@ -130,50 +135,67 @@ server(std::shared_ptr<coro::io_scheduler> executor, uint16_t Port) {
   }
 }
 
-static coro::task<void>
-client_handler(coro::ex_asio& ex, uint16_t Port, size_t Count) {
-  tcp::socket s(ex);
-  co_await s.async_connect({tcp::v4(), Port}, coro::aw_asio);
-
-  auto d = boost::asio::buffer(static_request);
-  char response_buf[4096];
-  size_t i = 0;
-  for (; i < Count; ++i) {
-    auto r = boost::asio::buffer(response_buf);
-    {
-      auto [err, n2] = co_await boost::asio::async_write(s, d, coro::aw_asio);
-      if (err) {
-        s.close();
-        break;
-      }
-    }
-    {
-      auto [err, n] = co_await s.async_read_some(r, coro::aw_asio);
-      if (err) {
-        s.close();
-        break;
-      }
-    }
-  }
-  if (!s.is_open()) {
-    std::printf("client finished early\n");
+static coro::task<void> client_handler(
+  std::unique_ptr<coro::io_scheduler>& executor, uint16_t Port, size_t Count
+) {
+  executor->schedule();
+  coro::net::tcp::client client{
+    executor, coro::net::tcp::client::options{.port = Port}
+  };
+  auto cstat = co_await client.connect();
+  if (cstat != coro::net::connect_status::connected) {
+    std::printf("failed to connect\n");
     std::terminate();
   }
 
-  s.shutdown(tcp::socket::shutdown_both);
-  s.close();
+  co_await client.poll(coro::poll_op::write);
+
+  std::string request_data(static_request);
+  std::string response_buf(4096, '\0');
+  size_t i = 0;
+  for (; i < Count; ++i) {
+    // Send request
+    co_await client.poll(coro::poll_op::write);
+    auto sspan = std::span<const char>{request_data};
+    coro::net::send_status sstatus;
+    do {
+      std::tie(sstatus, sspan) = client.send(sspan);
+      if (sstatus != coro::net::send_status::ok) {
+        client.socket().close();
+        co_return;
+      }
+    } while (!sspan.empty());
+
+    // Receive response
+    while (true) {
+      co_await client.poll(coro::poll_op::read);
+      auto [rstatus, rspan] = client.recv(response_buf);
+      switch (rstatus) {
+      case coro::net::recv_status::ok:
+        goto NEXT_ITER;
+      case coro::net::recv_status::try_again:
+        break;
+      default:
+        client.socket().close();
+        co_return;
+      }
+    }
+  NEXT_ITER:;
+  }
+
+  client.socket().shutdown();
+  client.socket().close();
 }
 
-static coro::task<void> client(coro::ex_asio& ex, uint16_t Port) {
-  size_t per_task = REQUEST_COUNT / CONNECTION_COUNT;
-  size_t rem = REQUEST_COUNT % CONNECTION_COUNT;
-  std::vector<coro::task<void>> clients(CONNECTION_COUNT);
+static coro::task<void>
+client(std::unique_ptr<coro::io_scheduler>& executor, uint16_t Port) {
   for (size_t i = 0; i < CONNECTION_COUNT; ++i) {
+    size_t per_task = REQUEST_COUNT / CONNECTION_COUNT;
+    size_t rem = REQUEST_COUNT % CONNECTION_COUNT;
     size_t count = i < rem ? per_task + 1 : per_task;
-    clients[i] = client_handler(ex, Port, count);
-    // co_await client_handler(ex, Port, count);
+    executor->spawn_detached(client_handler(executor, Port, count));
   }
-  co_await coro::spawn_many(clients.begin(), clients.end());
+  co_return;
 }
 
 int main(int argc, char* argv[]) {
@@ -184,14 +206,14 @@ int main(int argc, char* argv[]) {
     CONNECTION_COUNT = static_cast<size_t>(atoi(argv[1]));
   }
 
-  auto server_executor = coro::io_scheduler::make_shared(
+  auto server_executor = coro::io_scheduler::make_unique(
     coro::io_scheduler::options{
       .execution_strategy =
         coro::io_scheduler::execution_strategy_t::process_tasks_inline
     }
   );
 
-  auto client_executor = coro::io_scheduler::make_shared(
+  auto client_executor = coro::io_scheduler::make_unique(
     coro::io_scheduler::options{
       .execution_strategy =
         coro::io_scheduler::execution_strategy_t::process_tasks_inline
@@ -202,7 +224,7 @@ int main(int argc, char* argv[]) {
 
   auto startTime = std::chrono::high_resolution_clock::now();
   coro::sync_wait(
-    coro::when_all(server(server_executor), client(client_executor))
+    coro::when_all(server(server_executor, PORT), client(client_executor, PORT))
   );
   auto endTime = std::chrono::high_resolution_clock::now();
   auto totalTimeUs =
